5a4c051633beaeac477bd20adcf5547ed879c418
Evan Pendergraft <evanpendergraft@gmail.com>
2021-04-13 14:34:13 -0400

    added squirrel event handle

--- main.js
+++ main.js
@@ -1,8 +1,53 @@
 const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron')
+
 const {createOverlayFixWindow} = require('./src/modules/window/windows-taskbar-fix')
 const {createWindow, changedWindowSize} = require('./src/modules/window/create-window')
 const {createSystemTray} = require('./src/modules/window/system-tray')
 const {listenForAsyncButtonPress} = require('./src/modules/ipc/syncbuttonpress')
+
+
+// handle the squirrel installer events
+var handleStartupEvent = function() {
+  if (process.platform !== 'win32') {
+    return false;
+  }
+
+  var squirrelCommand = process.argv[1];
+  switch (squirrelCommand) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+
+      // Optionally do things such as:
+      //
+      // - Install desktop and start menu shortcuts
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Always quit when done
+      app.quit();
+
+      return true;
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Always quit when done
+      app.quit();
+
+      return true;
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+      app.quit();
+      return true;
+  }
+};
+
+if (handleStartupEvent()) {
+  return;
+}
 
 
 //things to do when app is ready
